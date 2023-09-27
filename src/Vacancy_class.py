d48038e073742f4fbc437e94da9f284de8483ff2
Катя <mochalova.es@mail.ru>
2023-09-27 22:37:22 +0300

    Создание класса с обозначением атрибутов вакансий

--- src/Vacancy_class.py
+++ src/Vacancy_class.py
@@ -1,4 +1,47 @@
-
-
-
 class Vacancy:
+    """
+    Класс для работы с вакансиями
+    """
+
+    def __init__(self, name_vacancy, url_vacancy, salary_vacancy, info_vacancy):
+        self.name_vacancy = name_vacancy
+        self.url_vacancy = url_vacancy
+        self.salary_vacancy = salary_vacancy
+        self.info_vacancy = info_vacancy
+
+    def __str__(self):
+        return f"""Вакансия: {self.name_vacancy}
+Ссылка на вакансию: {self.url_vacancy}  
+Предложения по зар.плате вакансии: {self.salary_vacancy}
+Информация по вакансии: {self.info_vacancy}      
+"""
+
+    def __gt__(self, other):
+        """
+        Определяет поведение оператора сравнения '>'
+        """
+        return int(self.salary_vacancy) > int(other.salary_vacancy)
+
+    def __ge__(self, other):
+        """
+        Определяет поведение оператора сравнения '>='
+        """
+        return int(self.salary_vacancy) >= int(other.salary_vacancy)
+
+    def __lt__(self, other):
+        """
+        Определяет поведение оператора сравнения '<'
+        """
+        return int(self.salary_vacancy) < int(other.salary_vacancy)
+
+    def __le__(self, other):
+        """
+        Определяет поведение оператора сравнения '<='
+        """
+        return int(self.salary_vacancy) <= int(other.salary_vacancy)
+
+    def __eq__(self, other):
+        """
+        Определяет поведение оператора сравнения '=='
+        """
+        return int(self.salary_vacancy) == int(other.salary_vacancy)
